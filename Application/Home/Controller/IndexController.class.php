9f05a4ce334ce6ce16b2be6c18a910cc84f7148c
mingmeng <i@yangruixin.com>
2019-04-11 01:04:20 +0800

    1

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -56,18 +56,13 @@ class IndexController extends Controller
         $isExist = $userModel->where(array("openid" => $openid))->count();
 
         if ($isExist == 1) {
-//            $payload = array(
-//                "openid" => $openid,
-//                "iss" => self::ISS,
-//                "iat" => time(),
-//                "nbf" => time() + 3600
-//            );
-//            $token = JWT::encode($payload, self::TOKEN_KEY);
-
             cookie("openid", $openid);
-//            cookie("_t", $token, array('expire' => 3600, 'httponly' => TRUE));
+            $timestamp = time() + 3600;
+            cookie("_e", $timestamp);
 
-            header("Location:" . FRONT_ENTRANCE . "?r=" . rand());
+            $token = sha1($openid . "redrock@lalala" . $timestamp);
+
+            cookie("_t", $token, array('expire' => 3600, 'httponly' => TRUE));
         } else {
             $data = getStuInfoByOpenid($openid);
 
@@ -120,7 +115,6 @@ class IndexController extends Controller
                 returnJson(500);
 
 
-//            $token = JWT::encode($payload, self::TOKEN_KEY);
             cookie("openid", $openid);
             $timestamp = time() + 3600;
             cookie("_e", $timestamp);
