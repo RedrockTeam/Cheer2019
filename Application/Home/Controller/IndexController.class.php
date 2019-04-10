9d7479935c9f4db529a428d08afa36ddd050da6f
mingmeng <i@yangruixin.com>
2019-04-10 17:03:36 +0800

    test userinfo

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -172,8 +172,10 @@ class IndexController extends Controller
         $voteRecords = $logModel
             ->where(array(
                 "userid" => $user["id"],
-                "time" => array("BETWEEN", array(date("Y-m-d 00:00:00", date("Y-m-d 23:59:59"))))
+                "time" => array("BETWEEN", array(date("Y-m-d 00:00:00"), date("Y-m-d 23:59:59")))
             ))->select();
+        var_dump($voteRecords);
+        exit;
 
         if (count($voteRecords) >= 5)
             returnJson(427, "no enough times to vote");
