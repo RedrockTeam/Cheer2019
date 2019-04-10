e47f8f5d333561ea9686f86c27766bc18e11921d
mingmeng <i@yangruixin.com>
2019-04-10 17:25:52 +0800

    bug fix

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -51,20 +51,22 @@ function getStuInfoByOpenid($openid)
     if (empty($openid))
         returnJson(403, "illegal openid!");
 
-    $ch = curl_init();
-    $options = array(
-        CURLOPT_URL => "https://wx.idsbllp.cn/MagicLoop/index.php?s=/addon/UserCenter/UserCenter/getStuInfoByOpenId&openId=" . $openid,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_HEADER => 0,
-    );
-    curl_setopt_array($ch, $options);
-    $isLegal = curl_exec($ch);
-    $isLegal = json_decode($isLegal);
-
-    if ($isLegal->status != 200)
-        returnJson(403, "illegal openid!");
+//    $ch = curl_init();
+//    $options = array(
+//        CURLOPT_URL => "https://wx.idsbllp.cn/MagicLoop/index.php?s=/addon/UserCenter/UserCenter/getStuInfoByOpenId&openId=" . $openid,
+//        CURLOPT_SSL_VERIFYPEER => false,
+//        CURLOPT_SSL_VERIFYHOST => false,
+//        CURLOPT_RETURNTRANSFER => 1,
+//        CURLOPT_POST => 1,
+//        CURLOPT_POSTFIELDS => http_build_query(array(""=>openid)),
+//        CURLOPT_HEADER => 0,
+//    );
+//    curl_setopt_array($ch, $options);
+//    $isLegal = curl_exec($ch);
+//    $isLegal = json_decode($isLegal);
+//
+//    if ($isLegal->status != 200)
+//        returnJson(403, "illegal openid!");
 
     $ch = curl_init();
     $options = array(
